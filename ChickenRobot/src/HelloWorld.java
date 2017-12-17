d39d0ecfa99aafa553aab1af948dafd715331b67
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2017-12-17 13:59:32 +0100

    Ajout d'un hello world.

---
+++ ChickenRobot/src/HelloWorld.java
@@ -0,0 +1,20 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.TextLCD;
+
+/**
+ * Example leJOS EV3 Project with an ant build file
+ */
+public class HelloWorld {
+	
+	public static void main(final String[] args) {
+		final EV3 ev3 = (EV3) BrickFinder.getLocal();
+		final TextLCD lcd = ev3.getTextLCD();
+		final Keys keys = ev3.getKeys();
+
+		lcd.drawString("Hello World", 4, 4);
+		keys.waitForAnyPress();
+	}
+
+}
