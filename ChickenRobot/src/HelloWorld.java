fca796951427c49cb3d51be692f5e6b524e22589
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-11 11:05:53 +0100

    Ajout d'un test des touch sensors

--- ChickenRobot/src/HelloWorld.java
+++ ChickenRobot/src/HelloWorld.java
@@ -1,20 +1,56 @@
-import lejos.hardware.BrickFinder;
-import lejos.hardware.Keys;
-import lejos.hardware.ev3.EV3;
-import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
 /**
  * Example leJOS EV3 Project with an ant build file
  */
 public class HelloWorld {
-	
-	public static void main(final String[] args) {
-		final EV3 ev3 = (EV3) BrickFinder.getLocal();
-		final TextLCD lcd = ev3.getTextLCD();
-		final Keys keys = ev3.getKeys();
 
-		lcd.drawString("Hello World", 4, 4);
-		keys.waitForAnyPress();
+	public static void main(final String[] args) {
+		touchSensorsTest();
 	}
 
+	private static void touchSensorsTest() {
+		//Create motors
+		final RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+		final RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
+
+		//Forward
+		leftMotor.setSpeed(720);// 2 RPM
+		rightMotor.setSpeed(720);
+		leftMotor.forward();
+		rightMotor.forward();
+		
+		//Touch sensor
+		final EV3TouchSensor touchSensor1 = new EV3TouchSensor(SensorPort.S1);
+		final EV3TouchSensor touchSensor2 = new EV3TouchSensor(SensorPort.S2);
+		final SensorMode touch1 = touchSensor1.getTouchMode();
+		final SensorMode touch2 = touchSensor2.getTouchMode();
+		final float[] sample = new float[2];
+		
+		//Wait for obstacle
+		do {
+			touch1.fetchSample(sample, 0);
+			touch2.fetchSample(sample, 1);
+		} while (leftMotor.isMoving() && rightMotor.isMoving() && (sample[0] == 0 || sample[1] == 0));
+		
+		//Go backwards
+		leftMotor.backward();
+		rightMotor.backward();
+		Delay.msDelay(1000);
+		leftMotor.stop();
+		rightMotor.stop();
+		
+		//Close resources
+		leftMotor.close();
+		rightMotor.close();
+		touchSensor1.close();
+		touchSensor2.close();
+	}
+	
 }
