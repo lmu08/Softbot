61b695733862cac69a575f7ba764861e4122bc23
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-15 19:43:50 +0100

    Ajout de package-info

---
+++ ChickenRobot/src/maze/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The maze package contains classes used to solve the maze and make the robot go through it.
+ *
+ * @version 1.0
+ */
+package maze;
---
+++ ChickenRobot/src/sensors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The sensors package contains classes representing all the sensors used by the robot.
+ * 
+ * @version 1.0
+ */
+package sensors;
